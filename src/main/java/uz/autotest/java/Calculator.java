b45879eb3139a7a7406f153943d9db44d8a74919
asldev <eshonkulov.asliddin.mars@gmail.com>
2026-02-02 08:06:20 +0500

    Add solution!

--- src/main/java/uz/autotest/java/Calculator.java
+++ src/main/java/uz/autotest/java/Calculator.java
@@ -3,10 +3,10 @@ package uz.autotest.java;
 public class Calculator {
 
     public int add(int x, int y) {
-        return 0;
+        return x + y;
     }
 
     public int subtract(int x, int y) {
-        return 0;
+        return x - y;
     }
 }
